337cb2a2e23d6b25f1648f090a85ebd4e51d871a
João Pedro Israel <juanpeterisrael@gmail.com>
2020-04-09 19:38:40 -0300

    Passou em todos os testes!

--- src/app.js
+++ src/app.js
@@ -1,33 +1,96 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
+function validadeProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({
+      "error": "Invalid project id"
+    })
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validadeProjectId);
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+  const likes = 0;
+
+  if(!url || !title || !techs) {
+    return response.status(400).json({"error": "You are missing one or more arguments for your repository"});
+  }
+
+  const repository = { id: uuid(), url, title, techs, likes};
+
+  repositories.push(repository);
+
+  return response.send(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs} = request.body;
+
+  if (request.body.likes) {
+    return response.status(400).json({"likes": 0});
+  }
+
+  if(!url || !title || !techs) {
+    return response.status(400).json({"error": "You are missing one or more arguments for your repository"});
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  const likes = repositories[repositoryIndex].likes;
+
+  const repository = {
+    id,
+    url,
+    title,
+    techs,
+    likes
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  const repository = repositories[repositoryIndex]
+
+  const likes = repositories[repositoryIndex].likes;
+
+  repositories[repositoryIndex].likes = likes + 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, (request,response) => {
+  return console.log("🚀🚀🚀Back-end Online!🚀🚀🚀")
+});
